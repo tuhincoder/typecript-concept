a5e659a2acc050896b06b5c13cc6b6a45de974db
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-08 16:37:15 +0600

    simple concept typescript practice

---
+++ module1/src/1.4.ts
@@ -0,0 +1,41 @@
+{
+    //
+    let firstName: string = 'tuhin';
+    let roll: number = 45;
+    let isAdmin: boolean = true;
+    let y: null = null;
+    // ------------
+    const name: string = 'tuhin';
+    const rollNumber: number = 34;
+    const isBuisnessman: boolean = true;
+    const x: null = null;
+
+    // -------------
+    let friends: string[] = ['tuhin', 'mahmudul']
+    let myFriends: string[] = ['mahmudul', 'hasan']
+    let myFriendsRollNumber: number[] = [1, 23, 455]
+    let othersFriends: string[] = ['parvej', 'hasib', 'akash']
+    let myBooks: number[] = [1, 2, 3, 45,]
+    let eligibleRollList: number[] = [1, 23, 4, 56]
+
+    // ----------------------
+    // tuple
+    const bestFriends: [string, string] = ['mahmudul', 'hasan'];
+    const oldFriends: [string, string, string] = ['torikul', 'islam', 'jabir']
+
+    let coordinate: [number, number] = [2, 4]
+    let ageName: [number, string] = [23, 'hasib']
+    ageName[0] = 400;
+    let newFriends: [string, string] = ['tuihn', 'mahmudul']
+    let myTeachersAndCountTe: [number, string, string, number, boolean] = [23, 'hasan', 'mahmud', 23, true]
+
+    let myBodySpryNumber: [string, number, string] = ['fogg1', 2, 'fogg2']
+
+
+
+
+
+
+    //
+
+}
