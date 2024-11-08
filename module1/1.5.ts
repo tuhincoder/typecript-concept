b05cf46c7ae9f8a17cb0c0703b632d5afe350108
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-11-08 16:50:10 +0600

    object type with typScript

---
+++ module1/1.5.ts
@@ -0,0 +1,23 @@
+{
+    //
+    const user: {
+        // company: 'BDThem'  ----> literal types
+        readonly company: string;
+        name: string;
+        age: number;
+        designation?: string; // ---> optional type
+        isMarried?: boolean;
+    } = {
+        company: 'BDThem',
+        name: 'mahmudul hasan tuhin',
+        age: 12,
+        designation: 'web developer',
+        isMarried: false,
+    }
+
+
+
+
+
+    //
+}
